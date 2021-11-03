49e71a24b464126d092e940d4cf2b235ca5f4293
FlorianMauduit <flomauduit@orange.fr>
2021-11-03 17:20:58 +0100

    Commentaire Ajouté

--- src/components/FormulaireCreationCocktails.jsx
+++ src/components/FormulaireCreationCocktails.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import './formulaireCreationCocktails.css';
 
+// Le Formulaire de Création de Cocktail
 function FormulaireCreationCocktails() {
   return (
     <div className="formulaire">
       <form>
         <h1>What about offering your own cocktail?</h1>
+        {/* Input pour le Nom du Cocktail */}
         <h3>Name of the Cocktail :</h3>
         <label htmlFor="nameofthecocktail">
           <input type="text" id="name" placeholder="Name of the cocktail" />
         </label>
+        {/* Input des Ingrédients */}
         <h3>Ingredient :</h3>
         <label htmlFor="nameofthecocktail">
           <input type="text" id="ingredient" placeholder="Ingredient" />
         </label>
+        {/* Input Instruction de Préparation */}
         <h3>Preparation :</h3>
         <label htmlFor="nameofthecocktail">
           <textarea
@@ -22,6 +26,7 @@ function FormulaireCreationCocktails() {
             placeholder="The preparation of cocktails"
           />
         </label>
+        {/* Button Envoyé */}
         <button type="submit" className="submitCocktails">
           suggest
         </button>
